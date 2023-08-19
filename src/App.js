2d7672a95df2ea0c07db3afa31b4019fdfa120a2
purpl3pineapple <opaloctopus1005@gmail.com>
2023-08-19 13:05:09 -0400

    router & page routes added to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Explorer from "./pages/Explorer";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import "./index.css";
+
 function App() {
   return (
     <>
-    <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explorer />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/profile" element={<SignIn />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+        </Routes>
+      </Router>
+      {/* Navbar */}
     </>
   );
-}
+};
 
 export default App;

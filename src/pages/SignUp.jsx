3240a389f45cf3e78d86ad532682df196055d00f
purpl3pineapple <opaloctopus1005@gmail.com>
2023-08-19 15:13:24 -0400

    add authentication to register new user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import VisibilityIcon from "../assets/svg/visibilityIcon.svg";
 
 const SignUp = () => {
-    const [showPassword, setShowPassword] = useState(false);
+
+        const [showPassword, setShowPassword] = useState(false);
 
         const [formData, setFormData] = useState({
             name: '',
@@ -24,6 +27,34 @@ const SignUp = () => {
             }));
         };
 
+        const on_submit = async (e) => {
+
+            e.preventDefault();
+
+            try {
+
+                const auth = getAuth();
+
+                const userCredential = await createUserWithEmailAndPassword(
+                    auth, 
+                    email, 
+                    password
+                );
+
+                const user = userCredential.user;
+
+                updateProfile(auth.currentUser, {
+                    displayName: name
+                });
+
+                navigate('/');
+
+            } catch(error){
+
+                console.log({error})
+            };
+        };
+
         return (
             <div className="pageContainer">
                 <header>
@@ -31,7 +62,7 @@ const SignUp = () => {
                         Welcome Back!
                     </p>
                 </header>
-                <form>
+                <form onSubmit={on_submit}>
                     <input 
                         type="text" 
                         className="nameInput" 

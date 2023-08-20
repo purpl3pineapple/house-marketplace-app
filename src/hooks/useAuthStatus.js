8d4fb8d19444d1b4843bebe34200444e4e80db2b
purpl3pineapple <opaloctopus1005@gmail.com>
2023-08-19 21:58:07 -0400

    create custom 'useAuthStatus' hook

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const useAuthStatus = () => {
+
+    const auth = getAuth();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [checkingStatus, setCheckingStatus] = useState(true);
+    const isMounted = useRef(true)
+
+    useEffect(() => {
+
+        if (isMounted){
+
+            onAuthStateChanged(auth, (user) => {
+
+                if (user) setLoggedIn(true);
+    
+                setCheckingStatus(false);
+            });
+        };
+
+        return () => isMounted.current = false;
+
+    }, [isMounted]);
+
+    return {
+        loggedIn,
+        checkingStatus
+    };
+};
+
+export default useAuthStatus;

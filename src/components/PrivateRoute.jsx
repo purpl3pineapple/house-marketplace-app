869781aadc8bced8a752b66b29a2a62a38cd9209
purpl3pineapple <opaloctopus1005@gmail.com>
2023-08-19 21:35:19 -0400

    add PrivateRoute component for authenticated users

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+const PrivateRoute = () => {
+
+    const loggedIn = false;
+
+    return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />;
+};
+
+export default PrivateRoute;

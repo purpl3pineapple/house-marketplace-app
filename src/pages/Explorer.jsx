04ac5a6fbac4f3f84855231817447ad8f83517cf
purpl3pineapple <opaloctopus1005@gmail.com>
2023-08-20 06:32:06 -0400

    explore page setup

--- src/pages/Explorer.jsx
+++ src/pages/Explorer.jsx
@@ -1,7 +1,35 @@
+import { Link } from 'react-router-dom';
+import RentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
+import SellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
+
 const Explorer = () => {
   return (
-    <div>
-      EXPLORER
+    <div className='explore'>
+      <header>
+        <p className="pageHeader"></p>
+      </header>
+
+      <main>
+        {/* Slider */}
+
+        <p className="exploreCategoryHeading">
+          Categories
+        </p>
+        <div className="exploreCategories">
+          <Link to='/category/rent'>
+            <img src={RentCategoryImage} alt="rent" className='exploreCategoryImg'/>
+            <p className="exploreCategoryName">
+              Places for rent
+            </p>
+          </Link>
+          <Link to='/category/sell'>
+            <img src={SellCategoryImage} alt="sell" className='exploreCategoryImg'/>
+            <p className="exploreCategoryName">
+              Places for sale
+            </p>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 };

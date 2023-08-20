19dc7af1e4980f5c475ce691670fb116cb992be0
purpl3pineapple <opaloctopus1005@gmail.com>
2023-08-20 13:35:11 -0400

    setup ListingItem component

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -3,7 +3,10 @@ import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg';
 import BedIcon from '../assets/svg/bedIcon.svg';
 import BathtubIcon from '../assets/svg/bathtubIcon.svg';
 
-const ListingItem = ({ listing, id }) => {
+const ListingItem = ({ listing, id, onDelete }) => {
+
+    const deleteListing = (listing, id) => {}
+
     return (
         <div className='categoryListing'>
             <Link 
@@ -11,11 +14,53 @@ const ListingItem = ({ listing, id }) => {
                 className='categoryListingLink'
             >
                 <img 
-                    src={listing.imgUrls[0]} 
+                    src={listing.imageUrls[0]} 
                     alt={listing.name}
                     className='categoryListingImg'
                 />
+                <div className="categoryListingDetails">
+                    <p className="categoryListingLocation">
+                        {listing.location}
+                    </p>
+                    <p className="categoryListingName">
+                        {listing.name}
+                    </p>
+                    <p className="categoryListingPrice">
+                        ${
+                            listing.offer === true 
+                            ? `${
+                                listing.discountedPrice
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                            }` 
+                            : `${
+                                listing.regularPrice
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                            }`
+                        }
+                        {listing.type === 'rent' && ' / Month'}
+                    </p>
+                    <div className="categoryListingInfoDiv">
+                        <img src={BedIcon} alt="bed" />
+                        <p className="categoryListingInfoText">
+                            {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : `1 Bedroom`}
+                        </p>
+                        <img src={BathtubIcon} alt="bath" />
+                        <p className="categoryListingInfoText">
+                            {listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : `1 Bathroom`}
+                        </p>
+                    </div>
+                </div>
             </Link>
+
+            {onDelete && (
+                <DeleteIcon 
+                    className='removeIcon' 
+                    fill='rgb(231,76,60)'
+                    onClick={() => deleteListing(listing.name, listing.id)}
+                />
+            )}
         </div>
     );
 };

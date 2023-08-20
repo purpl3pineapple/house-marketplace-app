4e8c92bb6a08b4c6311ba9493ea371fba85cc733
purpl3pineapple <opaloctopus1005@gmail.com>
2023-08-19 21:59:34 -0400

    add useAuthStatus hook to PrivateRoute

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import useAuthStatus from '../hooks/useAuthStatus';
+import Spinner from './Spinner';
 
 const PrivateRoute = () => {
 
-    const loggedIn = false;
+    const { loggedIn, checkingStatus } = useAuthStatus();
+
+    if(checkingStatus) return <Spinner />;
 
     return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />;
 };

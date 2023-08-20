2b25001e9f05d89a5d60c790bbc12806bfe17029
purpl3pineapple <opaloctopus1005@gmail.com>
2023-08-20 16:30:57 -0400

    start setup 'create-listing' page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import { useEffect, useState, useRef } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import Spinner from '../components/Spinner';
 
 const CreateListing = () => {
-  return (
-    <div>
-      CREATE LISTING
-    </div>
-  );
+
+    const [geolocationEnabled, setGeolocationEnabled] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const [formData, setFormData] = useState({
+        type: 'rent',
+        name: '',
+        bedrooms: 1,
+        bathrooms: 1,
+        parking: false,
+        furnished: false,
+        address: '',
+        offer: false,
+        regularPrice: 0,
+        discountedPrice: 0,
+        images: [],
+        latitude: 0,
+        longitude: 0
+    });
+
+    const auth = getAuth();
+    const navigate = useNavigate();
+    const isMounted = useRef(true);
+
+    useEffect(() => {
+        
+        if(isMounted){
+
+            onAuthStateChanged(auth, (user) => {
+
+                if (user) setFormData({...formData, userRef: user.uid});
+                else navigate('/sign-in');
+            });
+        };
+
+    }, [isMounted]);
+
+
+    if(loading) return <Spinner />;    
+
+    return (
+        <div>
+        CREATE LISTING
+        </div>
+    );
 };
 
 export default CreateListing;

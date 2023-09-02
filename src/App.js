23f3495b0f7cb0be21a9c93714f860297e9f1810
purpl3pineapple <opaloctopus1005@gmail.com>
2023-09-02 10:31:47 -0400

    add Listing page route to app

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
         </Routes>
         <Navbar />
       </Router>

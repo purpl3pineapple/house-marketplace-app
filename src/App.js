d8a50993b46d3810d9ede33bed8d4d61426a6110
purpl3pineapple <opaloctopus1005@gmail.com>
2023-09-02 12:03:41 -0400

    'Contact Landlord' page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
+import ContactLandlord from "./pages/ContactLandlord";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/create-listing" element={<CreateListing />} />
           <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+          <Route path="/contact/:landlordId" element={<ContactLandlord />} />
         </Routes>
         <Navbar />
       </Router>

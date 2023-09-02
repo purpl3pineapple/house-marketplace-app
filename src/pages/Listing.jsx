8bce1c8f67ca4d652941383d243a3e0f2fd7106f
purpl3pineapple <opaloctopus1005@gmail.com>
2023-09-02 17:04:29 -0400

    add Leaflet map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { db } from '../firebase.config';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import Spinner from '../components/Spinner';
 import ShareIcon from '../assets/svg/shareIcon.svg';
 
@@ -107,7 +108,30 @@ const Listing = () => {
 
                 <p className="listingLocationTitle">Location</p>
 
-                {/* MAP */}
+                <div className="leafletContainer">
+                    <MapContainer 
+                        style={{ height: '100%', width: '100%'}}
+                        center={[
+                            listing.geolocation.lat,
+                            listing.geolocation.lng
+                        ]}
+                        zoom={13}
+                        scrollWheelZoom={false}
+                    >
+
+                        <TileLayer 
+                            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                            url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+                        />
+
+                        <Marker position={[
+                            listing.geolocation.lat,
+                            listing.geolocation.lng
+                        ]}>
+                            <Popup>{listing.location}</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
 
                 {auth.currentUser?.uid !== listing.userRef && (
                     <Link 
